4953fb478560d9a9c4db6c57a34656317348a86c
Ulrich Johan Isachsen <ulrichji@stud.ntnu.no>
2017-01-24 14:36:43 +0100

    Wrote a script that runs the .cc files and runs the simulator

--- statrunner.py
+++ statrunner.py
@@ -1,6 +1,30 @@
+import subprocess
+import glob
+import os
 
-frameworkPath = "~/framework"
+#Path to the framework directory. Should be placed here.
+frameworkPath = "../"
+
+def runTest(file):
+	print("Running: "+' '.join(["cp",file,"prefetcher/prefetcher.cc"]))
+	subprocess.call(["cp",file,"prefetcher/prefetcher.cc"])
+	print("Running: "+"./compile.sh")
+	subprocess.call("./compile.sh")
+	print("Running: "+"python test_prefetcher.py")
+	proc = subprocess.Popen(["python","test_prefetcher.py"],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+	out,err = proc.communicate()
+	
+	print(out)
+
+def runTests():
+	os.chdir(frameworkPath)
+	fileList = []
+	for file in glob.glob(frameworkPath + "ComputerArchitecture/*.cc"):
+		fileList.append(file)
+	
+	for file in fileList:
+		runTest(file)
 
 def main():
-	pass
+	runTests()
 main()
