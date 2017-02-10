24ccaab4fcab6a9f83316c73003538d086dd58c6
Kristian Roaldsnes <kriroald@gmail.com>
2017-02-10 11:46:15 +0100

    Started implementing TCPrefetching using vectors. Assuming one-way set assosiative data cache.

--- tcpre.cc
+++ tcpre.cc
@@ -5,10 +5,49 @@
  */
 
 #include "interface.hh"
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+
+#define NUM_SETS_L1 1024
+#define K_entries   2
+#define SIZE_TAG    64 // bits, expressed in the usage of "uint64_t" in the vectors
+
+
+#define M_TAGBITS 7
+#define NUM_SETS_PHT (2*pow(2,7))
+#define NUM_ENTRIES_PHT 2
+
+// THT - Tag History Table
+vector< vector<uint64_t> > THT(NUM_SETS_L1,vector<uint64_t>(K_entries)); 
+
+
+// PHT - Pattern History Table
+vector< vector<uint64_t> > PHT(NUM_SETS_PHT,vector<uint64_t>(NUM_ENTRIES_PHT));
+
 
 
 void prefetch_init(void)
 {
+
+    // init THT to 0
+    for ( int i = 0; i < NUM_SETS_L1 ; i++ ) {
+        for ( int j = 0 ; j < K_entries ; j++) {
+            THT[i][j]=0;
+            DPRINTF(HWPrefetch, "THT element ");//%i, %i = %i\n ",i,j,THT[i][j]);
+        }
+    }
+
+
+    // init PHT to 0
+    for (int i = 0 ; i < NUM_SETS_PHT ; i ++ ) {
+        for ( int j = 0; j < NUM_ENTRIES_PHT; j++) {
+            PHT[i][j] = 0; 
+        }
+    }
+
     /* Called before any calls to prefetch_access. */
     /* This is the place to initialize data structures. */
 
@@ -17,16 +56,23 @@ void prefetch_init(void)
 
 void prefetch_access(AccessStat stat)
 {
-    /* pf_addr is now an address within the _next_ cache block */
-    Addr pf_addr = stat.mem_addr + BLOCK_SIZE;
+   
+    
+    //  Phase 1 - Update
 
-    /*
-     * Issue a prefetch request if a demand miss occured,
-     * and the block is not already in cache.
-     */
-    if (stat.miss && !in_cache(pf_addr)) {
-        issue_prefetch(pf_addr);
-    }
+
+
+
+
+
+
+    // Phase 2 - Lookup
+ 
+
+
+
+
+
 }
 
 void prefetch_complete(Addr addr) {
