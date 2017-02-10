3b4fad866e517b5a94be90cc6159cbbf0ec8f569
Kristian Roaldsnes <kriroald@gmail.com>
2017-02-10 16:24:14 +0100

    Working on first implementation stalled due to lack of understaindg of the original algorithm. Need to read closer

--- tcpre.cc
+++ tcpre.cc
@@ -11,13 +11,14 @@
 using namespace std;
 
 
-#define NUM_SETS_L1 1024
-#define K_entries   2
-#define SIZE_TAG    12  // bits
+#define NUM_SETS_L1     1024
+#define K_entries       2
+#define SIZE_TAG        12  // bits
 
 
-#define M_TAGBITS 7
-#define NUM_SETS_PHT (2*pow(2,7))
+#define M_TAGBITS       9 
+#define NUM_SETS_PHT   (2*pow(2,M_TAGBITS))
+#define NUM_WAYS_PHT    8
 #define NUM_ENTRIES_PHT 2
 
 // THT - Tag History Table
@@ -25,7 +26,7 @@ vector< vector<int> > THT(NUM_SETS_L1,vector<int>(K_entries));
 
 
 // PHT - Pattern History Table
-vector< vector<int> > PHT(NUM_SETS_PHT,vector<int>(NUM_ENTRIES_PHT));
+vector<vector<vector<int> > > PHT;
 
 /*
 Address bus width is 32 bits. Calculating the tags and indexes:
@@ -49,6 +50,16 @@ MAX_PHYS_MEM_SIZE variable, and could potentially be wrong.
 #define INDEX_MASK      0x3FF
 #define TAG_MASK        0xFFF
 
+#define OLDEST_TAG_POS 0
+#define NEWEST_TAG_POS 1
+
+static int maskBits(int number, int mask){
+
+    return number & mask;
+
+}
+
+
 void prefetch_init(void)
 {
 
@@ -62,9 +73,14 @@ void prefetch_init(void)
 
 
     // init PHT to 0
-    for (int i = 0 ; i < NUM_SETS_PHT ; i ++ ) {
-        for ( int j = 0; j < NUM_ENTRIES_PHT; j++) {
-            PHT[i][j] = 0; 
+    PHT.resize(NUM_SETS_PHT);
+    for (int i = 0 ; i < NUM_SETS_PHT ; i++ ) {
+        PHT[i].resize(NUM_WAYS_PHT);
+        for ( int j = 0; j < NUM_WAYS_PHT; j++) {
+            PHT[i][j].resize(2);
+            
+            PHT[i][j][0] = 0;
+            PHT[i][j][1] = 0;
         }
     }
 
@@ -77,17 +93,47 @@ void prefetch_init(void)
 void prefetch_access(AccessStat stat)
 {
    
-//   cout << "Acces requested, memory adress: " << hex <<stat.mem_addr << dec << " \n"; 
-    //  Phase 1 - Update
-    if ( stat.miss) {
-        // 1. calculate the miss index, update corresponding THT sequence
-        int missIndex = ( stat.mem_addr >> OFFSET_SIZE  ) & ( INDEX_MASK );   
-        THT[missIndex][0]=THT[missIndex][1];
-        THT[missIndex[1] = missIndex;
+    //   cout << "Acces requested, memory adress: " << hex <<stat.mem_addr << dec << " \n"; 
+    
+    
+    static long long int counter = 0;
+    //  Phase 1 - Update. Note that THT[index][0] is oldest netry, THT[index][1] is newest
+    if ( stat.miss && counter == 1) {
+        
+        int missIndex   = ( stat.mem_addr >> OFFSET_SIZE  ) & ( INDEX_MASK );   
+        int missTag     = ( stat.mem_addr >> (OFFSET_SIZE + INDEX_SIZE) ) & ( TAG_MASK);
+      
+        int prev_oldestTag = THT[missIndex][OLDEST_TAG_POS];
+        int prev_newestTag = THT[missIndex][NEWEST_TAG_POS]; 
 
-   
+        cout << "Address: "     << hex << stat.mem_addr << "\n";
+        cout << "missIndex: "   << hex << missIndex << "\n";
+        cout << "missTag : "    << hex << missTag << "\n";
+
+        // 1. update THT sequence
+        THT[missIndex][OLDEST_TAG_POS] = prev_newestTag;
+        THT[missIndex][NEWEST_TAG_POS] = missTag;
+
+
+
+          
+        // 2. use tag sequence and missIndex to select a set  in PHT
+        int tag0 = prev_newestTag;
+        int tag1 = prev_oldestTag;
+
+        int PHT_set =(tag0+tag1) >>  SIZE_TAG-M_TAGBITS; 
+        
+
+        // 3. search the set for newestTag
+        
+
+
+
+
    }
 
+
+    counter++;
 
 
 
