0c0a975d0416a1e8f221f5985a698110780dd080
Ulrich Johan Isachsen <ulrichji@stud.ntnu.no>
2017-03-02 10:36:33 +0100

    Updated the statrunner to work on other computers than mine.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Put this repo inside the framework/ folder to make the scripts function properly
+Put this repo inside the framework/ folder to make the scripts function properly. The framework directory is supplied as help to the exercise this
+project is about.
+
+The statrunner.py file is highly dependent on directory structure. In order for it to function properly, the repo must be named
+ComputerArchitecture and placed in the framework/ directory as described above. When running the file, the working directory must
+be framework/ (stand in the framework directory). Then run the file using "python ComputerArchitecture/statrunner.py". The program
+will then copy, compile and run all files that end with ".cc" (example: stridedPrefetcher.cc). The output of running the tests is
+put in ComputerArchitecture/run_results.txt. Please note that the file "prefetcher.cc" in the prefetcher/ directory will be overwritten
+on each run. Please move this file if it contains work in progress.
--- statrunner.py
+++ statrunner.py
@@ -1,29 +1,88 @@
 import subprocess
 import glob
 import os
+import sys
 
 #Path to the framework directory. Should be placed here.
-frameworkPath = "../"
+directory = os.getcwd()+"/"
+frameworkPath = directory
 
-def runTest(file):
-	print("Running: "+' '.join(["cp",file,"prefetcher/prefetcher.cc"]))
-	subprocess.call(["cp",file,"prefetcher/prefetcher.cc"])
-	print("Running: "+"./compile.sh")
-	subprocess.call("./compile.sh")
-	print("Running: "+"python test_prefetcher.py")
+def runTest(file,comment=''):
+	outputFile = "prefetcher/prefetcher.cc"
+	print("Running: "+' '.join(["cp",file,outputFile]))
+	subprocess.call(["cp",file,outputFile])
+	
+	compileProgram = "./compile.sh"
+	print("Running: "+compileProgram)
+	subprocess.call(compileProgram)
+	
+	testProgram = "python test_prefetcher.py"
+	print("Running: "+testProgram)
+	if(comment != ''):
+		print(comment)
 	proc = subprocess.Popen(["python","test_prefetcher.py"],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
 	out,err = proc.communicate()
 	
-	print(out)
+	return out,err
+	
+	#print(out)
+
+def extractFileName(file):
+	lastSlash = file.rfind("/")
+	return file[lastSlash+1:]
+
+def getRowValues(row):
+	return row.split()
+
+def extractData(text):
+	lines = text.split("\n")
+	columns = []
+	resultTable = []
+	numberOfDottedLines = 0
+	for line in lines:
+		if("------------" in line and numberOfDottedLines < 3):
+			numberOfDottedLines += 1
+		elif(numberOfDottedLines == 1):
+			columns = getRowValues(line)
+		elif(numberOfDottedLines > 0 and numberOfDottedLines < 3):
+			row = getRowValues(line)
+			resultTable.append(row)
+	
+	return columns,resultTable
+	
+def getTestText():
+	f = open(directory+"test.txt","r")
+	text = ""
+	for line in f:
+		text += line
+	f.close()
+	return text
 
 def runTests():
 	os.chdir(frameworkPath)
 	fileList = []
+	
+	resultFile = open(frameworkPath + "ComputerArchitecture/run_results.txt","w")
+	
 	for file in glob.glob(frameworkPath + "ComputerArchitecture/*.cc"):
 		fileList.append(file)
 	
+	fileCounter = 1
+	totalFiles = len(fileList)
+	
 	for file in fileList:
-		runTest(file)
+		testText = "Running test "+str(fileCounter)+" of "+str(totalFiles)+":"
+		print(testText)
+		fileCounter += 1
+		
+		output,error = runTest(file,comment=testText)
+		#output = getTestText()
+		fileName = extractFileName(file)
+		column,data = extractData(output)
+		resultFile.write(fileName + ":\n"+str(column)+"\n"+"\n".join(str(i)[1:len(str(i))-1] for i in data))
+		resultFile.write('\n\n')
+	
+	resultFile.close()
 
 def main():
 	runTests()
